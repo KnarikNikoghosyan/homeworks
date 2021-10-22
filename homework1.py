8ceccb9c96b580233f51ce6c086b83fa3f5334b5
KnarikNikoghosyan <92878344+KnarikNikoghosyan@users.noreply.github.com>
2021-10-22 19:18:48 +0400

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,8 @@
+#5
+print("Python is powerful...and fast; \n\tplays well with others; \nruns everywhere;is friendly & easy to learn")
+
+#4
+name = input("What is your name? ")
+year = int(input("When were you born? "))
+country = input("Where were you born? ")
+print(f"Hello.My name is {name} and I was born in {country} in {year}.")
