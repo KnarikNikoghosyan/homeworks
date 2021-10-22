1e0c38054f896b6fe3d47d3dc481f7f75dfca208
KnarikNikoghosyan <92878344+KnarikNikoghosyan@users.noreply.github.com>
2021-10-22 19:20:32 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,42 @@
+# problem 1
+a = float(input("the breadth of rectangle"))
+b = float(input("the lenght of rectangle"))
+area = a * b
+print(area)
+
+
+# problem 2
+q = float(input("the side of square"))
+area = q**2
+print(area)
+
+
+# problem 3
+x = 7
+y = -101
+z = 9645
+number1 = 45.7
+number2 = 16.5
+number3 = 5464.64
+name1 = "Ani"
+name2 = "David"
+name3 = "Sona"
+x1 = True
+x2 = False
+p1 = True - True
+w = 1.5 / 5.6
+n1 = "lenght" + "breadth"
+b1 = True + False
+num1 = 12 + 56 - 48
+f1 = 5.5 + 96.7 + 101.25
+st = "Spain"+"Britain"
+sub1 = "algebra" + "geometry" + "analysis"
+calc1 = (167 - 67)/10
+
+# problem 4
+print(type(x),type(y),type(z))
+print(type(number1),type(number2),type(number3))
+print(type(name1),type(name2),type(name3))
+print(type(x1),type(x2),type(p1),type(w),type(n1),type(b1))
+print(type(num1))
+print(type(f1),type(st),type(sub1),type(calc1))
